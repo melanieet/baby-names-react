f61ccfbb2f9c18ea35e8cb593fe8b6e666c5d879
melanieet <melanieet@users.noreply.github.com>
2020-05-25 17:46:25 +0100

    Create list of baby names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import babyNamesData from "./babyNamesData.json";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      {babyNamesData.map((babyName) => (
+        <li key={babyName.id}>
+          {babyName.name} - {babyName.sex === "f" ? "female" : "male"}
+        </li>
+      ))}
     </div>
   );
 }
